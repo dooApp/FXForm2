6e8c2b661a136e9c90ca3a79985777f91a481efe
Antoine Mischler <antoine@dooapp.com>
2011-08-26 18:05:17 +0200

    extract demo to its own module

--- demo/src/main/java/com/dooapp/fxform/TestEnum.java
+++ demo/src/main/java/com/dooapp/fxform/TestEnum.java
@@ -3,7 +3,6 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  * Neither the name of dooApp nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
@@ -12,67 +11,13 @@
 
 package com.dooapp.fxform;
 
-import com.dooapp.fxform.annotation.FormFactory;
-import com.dooapp.fxform.annotation.NonVisual;
-import com.dooapp.fxform.type.EnumProperty;
-import javafx.beans.property.*;
-import org.hibernate.validator.constraints.Email;
-
-import javax.validation.constraints.Min;
-
-
 /**
  * User: Antoine Mischler
- * Date: 09/04/11
- * Time: 22:53
+ * Date: 17/04/11
+ * Time: 00:13
  */
-public class DemoObject {
+public enum TestEnum {
 
-    private StringProperty name = new SimpleStringProperty();
-
-    private StringProperty mail = new SimpleStringProperty();
-
-    @NonVisual
-    private StringProperty shouldNoBeInTheForm = new SimpleStringProperty();
-
-    private BooleanProperty lucky = new SimpleBooleanProperty();
-
-    private EnumProperty<TestEnum> letter = new EnumProperty(TestEnum.class);
-
-    private IntegerProperty age = new SimpleIntegerProperty();
-
-    private DoubleProperty height = new SimpleDoubleProperty();
-
-    @FormFactory(DemoFactory.class)
-    private StringProperty factoryAnnotationOnField = new SimpleStringProperty();
-
-    private CustomType customType = new CustomType();
-
-    public String getName() {
-        return name.get();
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    @Email
-    public String getMail() {
-        return mail.get();
-    }
-
-    @Min(value = 5)
-    public int getAge() {
-        return age.get();
-    }
-
-    public TestEnum getLetter() {
-        return letter.get();
-    }
-
-    public void setLetter(TestEnum testEnum) {
-        letter.set(testEnum);
-    }
-
+    A, B, C, D, E
 
 }
