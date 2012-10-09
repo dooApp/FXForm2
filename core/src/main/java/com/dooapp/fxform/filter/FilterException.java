59f56b943c9902805417fe57071a8de7c60739c9
Antoine Mischler <antoine@dooapp.com>
2012-10-09 15:21:50 +0200

    Fix gradient syntax

---
+++ core/src/main/java/com/dooapp/fxform/filter/FilterException.java
@@ -0,0 +1,17 @@
+package com.dooapp.fxform.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created at 09/10/12 15:07.<br>
+ *
+ * @author Antoine Mischler <antoine@dooapp.com>
+ */
+public class FilterException {
+    /**
+     * The logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(FilterException.class);
+
+}
