6b6d59be8fdccc189b9130d681ba8f6bc741ad2a
Antoine Mischler <antoine@dooapp.com>
2012-10-09 17:15:02 +0200

    Update copyright

--- core/src/main/java/com/dooapp/fxform/filter/NonVisualFilter.java
+++ core/src/main/java/com/dooapp/fxform/filter/NonVisualFilter.java
@@ -1,8 +1,9 @@
 /*
- * Copyright (c) 2011, dooApp <contact@dooapp.com>
+ * Copyright (c) 2012, dooApp <contact@dooapp.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  * Neither the name of dooApp nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
