40fa2c9d4adeeda6fb85d4843c9141f944602f0e
Antoine Mischler <antoine@dooapp.com>
2013-11-26 12:03:07 +0100

    Issue #45
    I wish I have a Skin to bind my model with an existing Node

--- core/src/main/java/com/dooapp/fxform/view/skin/FXMLSkin.java
+++ core/src/main/java/com/dooapp/fxform/view/skin/FXMLSkin.java
@@ -13,19 +13,14 @@
 package com.dooapp.fxform.view.skin;
 
 import com.dooapp.fxform.FXForm;
-import com.dooapp.fxform.model.Element;
-import com.dooapp.fxform.view.FXFormNode;
-import com.dooapp.fxform.view.FXFormNodeWrapper;
-import com.dooapp.fxform.view.FXFormSkin;
 import com.dooapp.fxform.view.NodeCreationException;
-import javafx.beans.property.Property;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.logging.Level;
+import java.util.concurrent.Callable;
 import java.util.logging.Logger;
 
 /**
@@ -33,7 +28,7 @@ import java.util.logging.Logger;
  *
  * @author Antoine Mischler <antoine@dooapp.com>
  */
-public class FXMLSkin extends FXFormSkin {
+public class FXMLSkin extends NodeSkin {
 
     private final Logger logger = Logger.getLogger(FXMLSkin.class.getName());
 
@@ -51,10 +46,16 @@ public class FXMLSkin extends FXFormSkin {
         super(fxForm);
         this.url = url;
         this.controller = controller;
+        setOnCreateNode(new Callable<Node>() {
+            @Override
+            public Node call() throws Exception {
+                return loadFxml();
+            }
+        });
     }
 
-    @Override
-    protected Node createRootNode() throws NodeCreationException {
+
+    protected Node loadFxml() throws NodeCreationException {
         fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(url);
         fxmlLoader.setController(controller);
@@ -63,37 +64,6 @@ public class FXMLSkin extends FXFormSkin {
         } catch (IOException e) {
             throw new NodeCreationException(e.getMessage(), e);
         }
-    }
-
-    @Override
-    protected ElementNodes createElementNodes(Element element) {
-        FXFormNode label = lookupNode(element, FXForm.LABEL_ID_SUFFIX);
-        FXFormNode editor = lookupNode(element, FXForm.EDITOR_ID_SUFFIX);
-        FXFormNode tooltip = lookupNode(element, FXForm.TOOLTIP_ID_SUFFIX);
-        FXFormNode constraint = lookupNode(element, FXForm.CONSTRAINT_ID_SUFFIX);
-        return new ElementNodes(label, editor, tooltip, constraint);
-
-    }
-
-    @Override
-    protected void deleteElementNodes(ElementNodes elementNodes) {
-    }
-
-
-    private FXFormNode lookupNode(Element element, String suffix) {
-        Node node = getNode().lookup("#" + element.getName() + suffix);
-        if (node != null) {
-            Property property = fxForm.getPropertyProvider().getProperty(node);
-            if (property != null) {
-            return new FXFormNodeWrapper(node, property);
-            } else {
-                logger.log(Level.WARNING, "Unable to find the property to bind for " + node + "\n" +
-                        "Check that you configured the AdapterProvider correctly. See FXForm#setAdapterProvider");
-                return null;
-            }
-        }
-        else
-            return null;
     }
 
 }
---
+++ core/src/main/java/com/dooapp/fxform/view/skin/NodeSkin.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2013, dooApp <contact@dooapp.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of dooApp nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.dooapp.fxform.view.skin;
+
+import com.dooapp.fxform.FXForm;
+import com.dooapp.fxform.model.Element;
+import com.dooapp.fxform.view.FXFormNode;
+import com.dooapp.fxform.view.FXFormNodeWrapper;
+import com.dooapp.fxform.view.FXFormSkin;
+import com.dooapp.fxform.view.NodeCreationException;
+import javafx.beans.property.Property;
+import javafx.scene.Node;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * User: Antoine Mischler <antoine@dooapp.com>
+ * Date: 26/11/2013
+ * Time: 11:53
+ */
+public class NodeSkin extends FXFormSkin {
+
+    private final Logger logger = Logger.getLogger(NodeSkin.class.getName());
+
+    private Node node;
+
+    private Callable<Node> onCreateNode;
+
+    protected NodeSkin(FXForm fxForm) {
+        super(fxForm);
+    }
+
+    public NodeSkin(FXForm fxForm, Callable<Node> onCreateNode) {
+        super(fxForm);
+        setOnCreateNode(onCreateNode);
+    }
+
+    public void setOnCreateNode(Callable<Node> onCreateNode) {
+        this.onCreateNode = onCreateNode;
+    }
+
+    @Override
+    protected Node createRootNode() throws NodeCreationException {
+        if (node == null) {
+            try {
+                node = onCreateNode.call();
+            } catch (Exception e) {
+                throw new NodeCreationException(e);
+            }
+        }
+        return node;
+    }
+
+    @Override
+    protected ElementNodes createElementNodes(Element element) {
+        FXFormNode label = lookupNode(element, FXForm.LABEL_ID_SUFFIX);
+        FXFormNode editor = lookupNode(element, FXForm.EDITOR_ID_SUFFIX);
+        FXFormNode tooltip = lookupNode(element, FXForm.TOOLTIP_ID_SUFFIX);
+        FXFormNode constraint = lookupNode(element, FXForm.CONSTRAINT_ID_SUFFIX);
+        return new ElementNodes(label, editor, tooltip, constraint);
+
+    }
+
+    @Override
+    protected void deleteElementNodes(ElementNodes elementNodes) {
+    }
+
+    private FXFormNode lookupNode(Element element, String suffix) {
+        Node node = getNode().lookup("#" + element.getName() + suffix);
+        if (node != null) {
+            Property property = fxForm.getPropertyProvider().getProperty(node);
+            if (property != null) {
+                return new FXFormNodeWrapper(node, property);
+            } else {
+                logger.log(Level.WARNING, "Unable to find the property to bind for " + node + "\n" +
+                        "Check that you configured the PropertyProvider correctly. See FXForm#setPropertyProvider");
+                return null;
+            }
+        } else
+            return null;
+    }
+
+}
