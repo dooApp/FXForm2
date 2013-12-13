ef3511465c3da52f8b69b6d4227904198f7b9a7f
Bastien <bastien.billey@dooapp.com>
2013-12-13 17:42:25 +0100

    Fix Class Cast Exception on some generics types (cherry picked from commit 6ead000)

---
+++ core/src/test/java/com/dooapp/fxform/reflection/classcast/DeviceModel.java
@@ -0,0 +1,12 @@
+package com.dooapp.fxform.reflection.classcast;
+
+/**
+ * TODO write documentation<br>
+ * <br>
+ * Created at 13/12/13 16:59.<br>
+ *
+ * @author Bastien
+ */
+public class DeviceModel {
+
+}
