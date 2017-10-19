01ac18733530349a954a6d448328ab8a48899c2d
Vianney <vianney.depauw@dooapp.com>
2017-10-19 10:10:04 +0200

    Unexpected behavior when managing multiple sources with empty ListProperty #154

---
+++ core/src/test/java/com/dooapp/fxform/issues/issue154/Issue154CustomControl.java
@@ -0,0 +1,23 @@
+package com.dooapp.fxform.issues.issue154;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Control;
+
+public class Issue154CustomControl extends Control {
+    private ListProperty<Object> items = new SimpleListProperty(FXCollections.observableArrayList());
+
+    public ObservableList<Object> getItems() {
+        return items.get();
+    }
+
+    public ListProperty<Object> itemsProperty() {
+        return items;
+    }
+
+    public void setItems(ObservableList<Object> items) {
+        this.items.set(items);
+    }
+}
---
+++ core/src/test/java/com/dooapp/fxform/issues/issue154/Issue154Test.java
@@ -0,0 +1,86 @@
+package com.dooapp.fxform.issues.issue154;
+
+import com.dooapp.fxform.FXForm;
+import com.dooapp.fxform.JavaFXRule;
+import com.dooapp.fxform.TestUtils;
+import com.dooapp.fxform.view.property.DefaultPropertyProvider;
+import com.dooapp.fxform.view.property.PropertyProvider;
+import com.dooapp.fxform.view.skin.FXMLSkin;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * User: Antoine Mischler <antoine@dooapp.com>
+ * Date: 15/05/2017
+ * Time: 17:12
+ */
+public class Issue154Test {
+
+    @Rule
+    public JavaFXRule javaFXRule = new JavaFXRule();
+
+
+    class TestBean {
+        private ListProperty<Double> items = new SimpleListProperty(FXCollections.observableArrayList());
+
+        public ObservableList<Double> getItems() {
+            return items.get();
+        }
+
+        public ListProperty<Double> itemsProperty() {
+            return items;
+        }
+
+        public void setItems(ObservableList<Double> items) {
+            this.items.set(items);
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
+        DefaultPropertyProvider.addGlobalProvider(Issue154CustomControl.class, new PropertyProvider<Issue154CustomControl>() {
+            @Override
+            public Property getProperty(Issue154CustomControl control) {
+                return control.itemsProperty();
+            }
+        });
+
+        FXForm fxForm = new FXForm();
+        fxForm.setSkin(new FXMLSkin(fxForm, Issue154Test.class.getClassLoader().getResource("issues/issue154/issue154.fxml")));
+
+        TestBean picturedDiagnosis = new TestBean();
+        picturedDiagnosis.getItems().add(10d);
+        fxForm.setSource(picturedDiagnosis);
+        TestUtils.syncJavaFXThread();
+
+        TestBean picturedDiagnosis2 = new TestBean();
+        fxForm.setSource(picturedDiagnosis2);
+        TestUtils.syncJavaFXThread();
+
+        TestBean picturedDiagnosis3 = new TestBean();
+        fxForm.setSource(picturedDiagnosis3);
+        TestUtils.syncJavaFXThread();
+
+        Issue154CustomControl flowDisplayer = (Issue154CustomControl) fxForm.lookup("#items-form-editor");
+        flowDisplayer.getItems().add(20d);
+        TestUtils.syncJavaFXThread();
+
+        Assert.assertEquals(1, picturedDiagnosis.getItems().size());
+        Assert.assertEquals(10d, picturedDiagnosis.getItems().get(0).doubleValue(), 0.01);
+
+        Assert.assertEquals(0, picturedDiagnosis2.getItems().size());
+
+        Assert.assertEquals(1, picturedDiagnosis3.getItems().size());
+        Assert.assertEquals(20d, picturedDiagnosis3.getItems().get(1).doubleValue(), 0.01);
+    }
+}
+
+
